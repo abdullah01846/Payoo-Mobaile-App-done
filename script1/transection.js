459c70224876edf03f28d7a1faa4cd19c4b7e68f
abdullah01846 <mdabdullahh106@gmail.com>
2025-11-08 11:15:32 +0600

    project done

--- script1/transection.js
+++ script1/transection.js
@@ -1,7 +1,18 @@
 handleToggle('transection-history', 'none');
+handleToggle('usersInfo', 'none');
+handleToggle('pricIng','none')
+// handleToggle('users-info','none');
 document.getElementById('transection-box').addEventListener('click',function(){
     handleToggle('addMoney', 'none');
     handleToggle('cashout', 'none');
      handleToggle('transection-history', 'block');
    
 })
+
+
+document.getElementById('users-info').addEventListener('click', function(){
+    handleToggle('usersInfo', 'block');
+    handleToggle('addMoney', 'none');
+    handleToggle('cashout', 'none');
+})
+
--- utilities/toggolle.js
+++ utilities/toggolle.js
@@ -1,13 +1,27 @@
 document.getElementById('cashout').style.display = 'none';
 
+
 document.getElementById('add-money-box').addEventListener('click', function(){
     handleToggle('addMoney', 'block')
     handleToggle('cashout', 'none')
     handleToggle('transection-history', 'none');
+    handleToggle('usersInfo', 'none');
+    handleToggle('pricIng','none')
 })
 
 document.getElementById('cashout-box').addEventListener('click', function(){
     handleToggle('addMoney', 'none')
     handleToggle('cashout', 'block')
     handleToggle('transection-history','none');
+    handleToggle('usersInfo', 'none');
+    handleToggle('pricIng','none')
+})
+
+
+document.getElementById('pricing').addEventListener('click', function(){
+    handleToggle('addMoney', 'none')
+    handleToggle('cashout', 'none')
+    handleToggle('transection-history','none');
+    handleToggle('usersInfo', 'none');
+    handleToggle('pricIng','block')
 })
