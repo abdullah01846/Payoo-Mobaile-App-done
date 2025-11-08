e84768a94af21552047d941ee33df10b7593c9ff
abdullah01846 <mdabdullahh106@gmail.com>
2025-11-08 22:57:38 +0600

    done

--- script1/transection.js
+++ script1/transection.js
@@ -1,6 +1,7 @@
 handleToggle('transection-history', 'none');
 handleToggle('usersInfo', 'none');
 handleToggle('pricIng','none')
+handleToggle('trans','none')
 // handleToggle('users-info','none');
 document.getElementById('transection-box').addEventListener('click',function(){
     handleToggle('addMoney', 'none');
@@ -14,5 +15,16 @@ document.getElementById('users-info').addEventListener('click', function(){
     handleToggle('usersInfo', 'block');
     handleToggle('addMoney', 'none');
     handleToggle('cashout', 'none');
+    handleToggle('transection-history', 'none');
+})
+
+
+
+document.getElementById('transfer-box').addEventListener('click', function(){
+    handleToggle('addMoney', 'none')
+    handleToggle('cashout', 'none')
+    handleToggle('transection-history','none');
+    handleToggle('usersInfo', 'none');
+    handleToggle('trans','block')
 })
 
